09cb6339d6f9fb8ed7b735d7f719d51c566fbabc
Evan Palubinski <116685993+DJSALADD@users.noreply.github.com>
2024-04-21 10:58:29 -0400

    Update gettingUserPhoto.py

--- gettingUserPhoto.py
+++ gettingUserPhoto.py
@@ -7,7 +7,7 @@ API_KEY = "AIzaSyDeqX5aOW5LEJVQQn-COQMO6BgcjfTismc"
 
     
 def getSearchImage(SEARCH_QUERY):
-
+    SEARCH_QUERY = SEARCH_QUERY+" regular food"
     url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx=714eb691b3bfd47ae&q={SEARCH_QUERY}&searchType=image"
 
     imageUrl = ""
