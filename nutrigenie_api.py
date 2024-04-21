71296279e03410c84a81e0ea2482f4cc195f5191
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-04-21 09:50:41 -0400

    renamed food recommender to nutrigenie added search image get method

--- nutrigenie_api.py
+++ nutrigenie_api.py
@@ -1,7 +1,6 @@
-import sys
-import os
 from fastapi import FastAPI
 from food_recommender import recommend_food
+from gettingUserPhoto import getSearchImage
 
 app = FastAPI()
 
@@ -9,3 +8,8 @@ app = FastAPI()
 async def get_recommendations(food_name: str, number_of_recommendations: int):
     recommendations = recommend_food(food_name, number_of_recommendations)
     return {"recommendations": recommendations}
+
+@app.get("/search_image/")
+async def get_search_image(query: str):
+    image_url = getSearchImage(query)
+    return {"image_url": image_url}
