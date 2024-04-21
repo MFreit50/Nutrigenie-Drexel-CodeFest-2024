bc18f7a6cbda27b44852971dac5519fd96a4b899
Denys Byrchak <db3558@drexel.edu>
2024-04-21 11:57:34 -0400

    Added picture analysis

--- nutrigenie_api.py
+++ nutrigenie_api.py
@@ -4,7 +4,6 @@ from gettingUserPhoto import getSearchImage
 
 app = FastAPI()
 
-
 @app.get("/recommendations/")
 async def get_recommendations(food_name: str, number_of_recommendations: int):
     recommendations = recommend_food(food_name, number_of_recommendations)
@@ -15,3 +14,23 @@ async def get_recommendations(food_name: str, number_of_recommendations: int):
 async def get_search_image(query: str):
     image_url = getSearchImage(query)
     return {"image_url": image_url}
+
+@app.post("/picture/")
+async def analyze_image(file: UploadFile = File(...)):
+    KEY = "mVkygYer.GwE3M2FHfVR2nygpqEiCejHQsp8niPIS"
+
+    file.filename = f"{uuid.uuid4()}.jpg"
+    img = await file.read()
+
+
+
+    url = "https://vision.foodvisor.io/api/1.0/en/analysis/"
+    headers = {"Authorization": f"Api-Key {KEY}"}
+
+    response = requests.post(url, headers=headers, files={"image": img})
+    response.raise_for_status()
+    data = response.json()
+
+    foodArray = [item["food"][0]["food_info"]["display_name"] for item in data["items"] if item["food"]]
+
+    return { "foods", foodArray }
