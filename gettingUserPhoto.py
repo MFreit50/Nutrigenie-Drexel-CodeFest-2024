6e4412f57645851243983fae2161767f8fdf1b37
Evan Palubinski <116685993+DJSALADD@users.noreply.github.com>
2024-04-20 21:13:54 -0400

    Add files via upload

---
+++ gettingUserPhoto.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+
+
+API_KEY = "AIzaSyDeqX5aOW5LEJVQQn-COQMO6BgcjfTismc"
+
+    
+def getSearchImage(SEARCH_QUERY):
+
+    url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx=714eb691b3bfd47ae&q={SEARCH_QUERY}&searchType=image"
+
+    imageUrl = ""
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        imageUrl = data["items"][0]["link"]
+    else:
+        print("Error:", response.status_code, response.text)
+    return imageUrl
+
+#Example call:
+print(getSearchImage("White mushrooms"))
