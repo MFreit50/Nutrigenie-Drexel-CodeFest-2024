a9c3e6d5a8d7a4572de4b23d94deed2c68083359
nebjamin <jonb1@newpaltz.edu>
2024-05-30 22:41:38 -0400

    Automatically send and handle a get request to root endpoint to wake on render server

--- app/App.jsx
+++ app/App.jsx
@@ -1,3 +1,5 @@
+
+import axios from 'axios';
 import {
   SafeAreaView,
   ScrollView,
@@ -7,8 +9,7 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -16,11 +17,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home'
 import Welcome from './components/Welcome'
 import Camera from './components/Camera'
-import Rundown from './components/Rundown'
+import Rundown from './components/Breakdown'
 import Nutrition from './components/Nutrition'
 import Alternatives from './components/Alternatives'
-
-//const [nutritionFacts, setNutritionFacts] = useState({});
+import { FoodsContext } from './contexts/FoodsContext'
 
 const Stack = createNativeStackNavigator();
 
@@ -29,21 +29,36 @@ const App = () => {
   const [ foods, setFoods ] = useState([]); // ["apple, "banana", "orange]
   const [recommendations, setRecommendations] = useState([{name: "apple", score: 0.5}, {name: "banana", score: 0.3}, {name: "orange", score: 0.2}]);
 
+  useEffect(() => {
+    const wakeUpServer = async () => {
+      try {
+        const url = 'https://drexel-codefest-2024-3.onrender.com'
+        const endpoint = '/'
+        const response = await axios.get(`${url}${endpoint}`)
+        console.log("RESPONSE", response.data)
+      } catch (error) {
+        console.log("ERROR", error)
+      }
+    }
+    wakeUpServer()
+  }, [])
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen 
-          name="Camera" 
-          component={Camera} 
-          setFoods={setFoods} 
-        />
-        <Stack.Screen name="Rundown" component={Rundown} foods={foods}/>
-        <Stack.Screen name="Nutrition" component={Nutrition} />
-        <Stack.Screen name="Alternatives" component={Alternatives} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <FoodsContext.Provider value={{ foods, setFoods }}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Welcome" component={Welcome} />
+          <Stack.Screen 
+            name="Camera" 
+            component={Camera}  
+          />
+          <Stack.Screen name="Rundown" component={Rundown} foods={foods}/>
+          <Stack.Screen name="Nutrition" component={Nutrition} />
+          <Stack.Screen name="Alternatives" component={Alternatives} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </FoodsContext.Provider>
   );
 }
 
